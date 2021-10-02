2089023d26215062770f18a23bae5fd4fd39ec28
Emiliano Mancuso <emiliano07.mp@gmail.com>
2021-10-03 00:34:20 +0200

    Add TP2

---
+++ TP2 - CLI/TP2-CLI.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+
+#RETO IV
+
+parser = argparse.ArgumentParser(description='Este es un saludador personalizado: ¡Toma tu nombre y apellido y te responde!')
+parser.add_argument('-n', '--name',
+                    type=str,
+                    help='user name',
+                    required=True)
+parser.add_argument('-s', '--surname',
+                    type=str,
+                    help='user surname',
+                    required=True)
+
+args = parser.parse_args()
+
+print(f"¡Hola {args.name} {args.surname}! ¡Bienvendix!")
